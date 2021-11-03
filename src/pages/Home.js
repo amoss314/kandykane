92149a3f766c0d674e1987b5dc7ffd97255b15ce
Ashley <me@domain.com>
2021-11-02 19:03:57 -0500

    unit test complete

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,12 @@
 import HomeStyle from '../components/Home.style'
 
 
-
-
 function HomePage() {
     return <div>
+        <header>Prolific Creations</header>
+        
       <HomeStyle />
-    </div>
+          </div>
     
   }
   
---
+++ src/pages/Home.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import HomePage from './Home';
+
+test('renders Prolific Creations as a text', () => {
+  render(<HomePage />);
+
+  const prolificCreationsElement = screen.getByText('Prolific Creations');
+  expect(prolificCreationsElement).toBeInTheDocument();
+});
